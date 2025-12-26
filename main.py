f80715578bde7a57a52fddf3e3909a38ca8a9990
mertsariyar <mertsariyar2@gmail.com>
2025-12-26 18:19:28 +0300

    added api key and input-ouput for ai agent

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if not api_key:
+    raise RuntimeError("api couldn't find try again.!")
+
 def main():
     print("Hello from my-ai!")
+    client = genai.Client(api_key=api_key)
+    propmt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=propmt)
+    print(response.text)
 
 
 if __name__ == "__main__":
