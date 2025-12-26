9994c656d84b68dacc9ce142a1ab6d752a36cc54
mertsariyar <mertsariyar2@gmail.com>
2025-12-26 20:39:15 +0300

    added argparse module for checking the user's input

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+import argparse
 import os
 from google import genai
 from dotenv import load_dotenv
 
-load_dotenv()
 
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if not api_key:
-    raise RuntimeError("api couldn't find try again.!")
 
 def main():
-    print("Hello from my-ai!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+   
+    
+    parser = argparse.ArgumentParser(description="chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    if not api_key:
+        raise RuntimeError("api couldn't find try again.!")
     client = genai.Client(api_key=api_key)
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
     if not response.usage_metadata:
         raise RuntimeError("Api Response Failure")
-    print(f"User propmt: {prompt}")
+    
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
